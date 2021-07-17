cf748257a9845b9d04322ab15b6463ada6bc4a77
Liana Farid <azlin.adliana@gmail.com>
2021-07-17 12:29:01 +0800

    inserted player image

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 # initialises the game
 pygame.init()
 
-# creates the screen
+# creates the screen (w x h)
 screen = pygame.display.set_mode( (800, 600))
 
 running = True
@@ -11,17 +11,27 @@ running = True
 #changing Title of the game 
 icon = pygame.image.load("ufo.png")
 pygame.display.set_icon(icon)
-
 pygame.display.set_caption("Space Invaders")
 
+# Player details
+playerImg = pygame.image.load("player.png")
+# sets where the image wants to be set (position)
+playerX = 370
+playerY = 480
+
+def player():
+    # blit means to draw, parameters is ( image, coordinates)
+    screen.blit(playerImg, (playerX,playerY))
 
 
 # closes the screen when we quit, if dont have this, then the screen is open forever
 while running :
+    # changing colour of background
+    screen.fill( (177, 156, 217))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    # changing colour of background
-    screen.fill( (177, 156, 217))
+    
+    player()
     pygame.display.update()
