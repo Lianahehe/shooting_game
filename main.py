327ef9768319e02ac15474cdd3182b45dc068bb4
Liana Farid <azlin.adliana@gmail.com>
2021-07-17 12:12:29 +0800

    set title and logo of game

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ screen = pygame.display.set_mode( (800, 600))
 
 running = True
 
+#changing Title of the game 
+icon = pygame.image.load("ufo.png")
+pygame.display.set_icon(icon)
+
+pygame.display.set_caption("Space Invaders")
+
+
+
 # closes the screen when we quit, if dont have this, then the screen is open forever
 while running :
     for event in pygame.event.get():
