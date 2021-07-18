55ef675fe4c5ed91e069cb4dc022d5ed5d7e41f2
Liana Farid <azlin.adliana@gmail.com>
2021-07-18 17:18:12 +0800

    ship move left and right

--- main.py
+++ main.py
@@ -11,17 +11,18 @@ running = True
 #changing Title of the game 
 icon = pygame.image.load("ufo.png")
 pygame.display.set_icon(icon)
-pygame.display.set_caption("Space Invaders")
+pygame.display.set_caption("Space Shooter")
 
 # Player details
 playerImg = pygame.image.load("player.png")
 # sets where the image wants to be set (position)
 playerX = 370
 playerY = 480
+playerX_change = 0
 
-def player():
+def player(x,y):
     # blit means to draw, parameters is ( image, coordinates)
-    screen.blit(playerImg, (playerX,playerY))
+    screen.blit(playerImg, (x,y) )
 
 
 # closes the screen when we quit, if dont have this, then the screen is open forever
@@ -32,6 +33,17 @@ while running :
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        # checks if key pressed is right or left (only works on arrow keys)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
+                playerX_change = 0
     
-    player()
+    playerX += playerX_change
+    player(playerX, playerY)
     pygame.display.update()
